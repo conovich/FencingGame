221e2523a18c6c63ec2ea724b6a43d600f98ae45
conovich <conovich@seas.upenn.edu>
2014-02-13 21:04:11 -0500

    Committed Image Effects

---
+++ Assets/Scripts/JoystickInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("A Button")){
+			Debug.Log("YAY A");
+		}
+		if(Input.GetButtonDown("B Button")){
+			Debug.Log("YAY B");
+		}
+		if(Input.GetButtonDown("X Button")){
+			Debug.Log("YAY X");
+		}
+		if(Input.GetButtonDown("Y Button")){
+			Debug.Log("YAY Y");
+		}
+		if(Input.GetAxis("X Axis") > 0){
+			Debug.Log("HEYO +X AXIS");
+		}
+		if(Input.GetAxis("X Axis") < 0){
+			Debug.Log("HEYO -X AXIS");
+		}
+	}
+}
