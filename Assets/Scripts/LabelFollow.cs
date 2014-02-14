2864fd87524707ca08e99107e2a5acf920d364c1
conovich <conovich@seas.upenn.edu>
2014-02-13 22:02:45 -0500

    Added fencer label following capabilities.

---
+++ Assets/Scripts/LabelFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabelFollow : MonoBehaviour {
+	public Transform _Target;
+	public Vector3 _Offset;
+
+	Vector3 targetScreenPoint;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GetTargetScreenPoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetTargetScreenPoint();
+		SetPosition();
+	}
+
+	void GetTargetScreenPoint(){
+		if(_Target){
+			targetScreenPoint = Camera.main.WorldToViewportPoint(_Target.position);
+		}
+	}
+
+	void SetPosition(){
+		transform.position = targetScreenPoint + _Offset;
+	}
+}
