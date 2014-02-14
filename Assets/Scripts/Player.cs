f60c5222e4e41b225e64b6c27be8934d370d5f55
conovich <conovich@seas.upenn.edu>
2014-02-13 21:22:49 -0500

    Integrated XBOX Controller.
    Player can now use XBOX controller to move the fencer.
    Fixed bug where the animation would restart with each input, now waits
    for animation to finish.
    [Also applied anti-aliasing image effect.]

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -70,8 +70,8 @@ public class Player : MonoBehaviour {
 		
 		if(!_inStartSequence){
 			_changeInDistance = _hips.position - _MyReference.position;;
-			Debug.Log("hips: " + _hips.position.ToString());
-			Debug.Log("MyReferencePosition!: " + _MyReference.position.ToString());
+			//Debug.Log("hips: " + _hips.position.ToString());
+			//Debug.Log("MyReferencePosition!: " + _MyReference.position.ToString());
 			if(gameObject.tag == "Player"){
 				GetInputPlayer();	
 			}
@@ -85,50 +85,57 @@ public class Player : MonoBehaviour {
 	}
 	
 	private void GetInputPlayer(){
-		if(Input.GetKey (KeyCode.RightArrow)){
-			PlayMyAnimation("Advance 1", State.advance);
-		}
-		else if(Input.GetKey (KeyCode.LeftArrow)){
-			PlayMyAnimation("Retreat", State.retreat);
-		}
-		else if(Input.GetKey (KeyCode.Space)){
-			PlayMyAnimation("LungeRecover", State.lungeRecover);
-		}
-		else if(Input.GetKey (KeyCode.Q)){
-			PlayMyAnimation("ParryOne", State.lungeRecover);
-		}
-		//not yet in the FENCER animations
-		/*else if(Input.GetKey (KeyCode.Keypad4)){
-			PlayMyAnimation("ParryFourNoExt", State.lungeRecover);
-		}*/
-		else if(Input.GetKey (KeyCode.W)){
-			PlayMyAnimation("ParrySix", State.lungeRecover);
-		}
-		else if(Input.GetKey (KeyCode.E)){
-			PlayMyAnimation("ParrySeven", State.lungeRecover);
-		}
-		//not working with FENCER
-		/*
-		else if(Input.GetKey (KeyCode.R)){
-			PlayMyAnimation("ParryEight", State.lungeRecover);
-		}*/
-		else{
-			_CurrentState = State.idle;	
+		if(!animation.isPlaying){
+			if(Input.GetKey (KeyCode.RightArrow) || Input.GetAxis("X Axis") > 0){
+				PlayMyAnimation("Advance 1", State.advance);
+			}
+			else if(Input.GetKey (KeyCode.LeftArrow) || Input.GetAxis("X Axis") < 0){
+				PlayMyAnimation("Retreat", State.retreat);
+			}
+			else if(Input.GetKey (KeyCode.Space) || Input.GetButtonDown("A Button")){
+				PlayMyAnimation("LungeRecover", State.lungeRecover);
+			}
+			else if(Input.GetKey (KeyCode.Q) || Input.GetButtonDown("Y Button")){
+				PlayMyAnimation("ParryOne", State.lungeRecover);
+			}
+			//not yet in the FENCER animations
+			/*else if(Input.GetKey (KeyCode.Keypad4)){
+				PlayMyAnimation("ParryFourNoExt", State.lungeRecover);
+			}*/
+			else if(Input.GetKey (KeyCode.W) || Input.GetButtonDown("B Button")){
+				PlayMyAnimation("ParrySix", State.lungeRecover);
+			}
+			else if(Input.GetKey (KeyCode.E)){
+				PlayMyAnimation("ParrySeven", State.lungeRecover);
+			}
+			else if(Input.GetKey (KeyCode.R) || Input.GetButtonDown("X Button")){
+				PlayMyAnimation("ParrySeven", State.lungeRecover);
+			}
+			//not working with FENCER
+			/*
+			else if(Input.GetKey (KeyCode.R)){
+				PlayMyAnimation("ParryEight", State.lungeRecover);
+			}*/
+			else{
+				_CurrentState = State.idle;	
+			}
 		}
 	}
 	
 	private void GetInputOpponent(){
-		if(_thePlayer._CurrentState == Player.State.advance){
-			PlayMyAnimation("Retreat", State.retreat);
-		}
-		else if(_thePlayer._CurrentState == Player.State.retreat){
-			PlayMyAnimation("Advance 1", State.advance);
-		}
-		else if(_thePlayer._CurrentState == Player.State.lungeRecover){
-			PlayMyAnimation("DoubleRetreat", State.retreat);
-		}
-		else if(_thePlayer._CurrentState == Player.State.idle){
-			
+		if(!animation.isPlaying){
+			if(_thePlayer._CurrentState == Player.State.advance){
+				PlayMyAnimation("Retreat", State.retreat);
+			}
+			else if(_thePlayer._CurrentState == Player.State.retreat){
+				PlayMyAnimation("Advance 1", State.advance);
+			}
+			else if(_thePlayer._CurrentState == Player.State.lungeRecover){
+				PlayMyAnimation("DoubleRetreat", State.retreat);
+			}
+			else if(_thePlayer._CurrentState == Player.State.idle){
+				
+			}
 		}
 	}
 	
@@ -142,7 +149,7 @@ public class Player : MonoBehaviour {
 		
 			_MyReference.position = _hips.position;
 			_currentDistance += new Vector3(0.0f, 0.0f, _changeInDistance.z);
-			Debug.Log(_currentDistance.ToString());
+			//Debug.Log(_currentDistance.ToString());
 			transform.position += _currentDistance;
 		}
 		else{
@@ -153,7 +160,7 @@ public class Player : MonoBehaviour {
 			
 			_MyReference.position = _hips.position;
 			_currentDistance += new Vector3(0.0f, 0.0f, _changeInDistance.z);
-			Debug.Log(_currentDistance.ToString());
+			//Debug.Log(_currentDistance.ToString());
 			transform.position += _currentDistance;
 		}
 	}
