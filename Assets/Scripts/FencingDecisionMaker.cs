a4f367589a8a9adc3ee107d6380c8928acb42036
conovich <conovich@seas.upenn.edu>
2014-02-16 16:22:22 -0500

    Added basic decision tree script.
    Creates action items from the available list of animations, then a
    manual network of possibilities is created. Then, a random action can be
    chosen from the available possibilities.

--- Assets/Scripts/FencingDecisionMaker.cs
+++ Assets/Scripts/FencingDecisionMaker.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//going to have to add multiple layers. brainstormed ways:
+//1) pointers so that you only ever create one of each type, next levels point to location of originals
+//2) make a new one every time...
+//3) make one of each type at the very beginning, copy this type every time...
+
 public class FencingDecisionMaker : MonoBehaviour {
-	public Animation _Animations;
+	public ArrayList _actionKeys = new ArrayList();
 
-	Action[] _myActions;
-
-	class Action{
-		public Action[] _PossibilitySpace;
+	public class Action{
+		public ArrayList _PossibilitySpace;
 		public string name;
 
-		Action(){
-
+		public Action(){
+			_PossibilitySpace = new ArrayList();
+		}
+		public Action(string newName){
+			name = newName;
+			_PossibilitySpace = new ArrayList();
 		}
 	}
 	// Use this for initialization
 	void Start () {
-	
+		CreateActionKeys();
+		MapActionKeys();
 	}
 	
 	// Update is called once per frame
@@ -24,8 +32,49 @@ public class FencingDecisionMaker : MonoBehaviour {
 	
 	}
 
-	void CreateActions(){
+	void CreateActionKeys(){
+		foreach(AnimationState anim in animation){
+			_actionKeys.Add(new Action(anim.name));
+		}
+	}
 
+	void MapActionKeys(){
+		for(int i = 0; i < _actionKeys.Count; i++){
+			Action currentAction = (Action)_actionKeys[i];
+			
+			switch (currentAction.name){
+			case "Advance":
+				//vvvadd something like below to check if the action is in the action keys!vvv
+				//if(_actionKeys.Contains(..
+				currentAction._PossibilitySpace.Add(new Action("Retreat"));
+				currentAction._PossibilitySpace.Add(new Action("DoubleRetreat"));
+				currentAction._PossibilitySpace.Add(new Action("LungeRecover"));
+				break;
+			case "Retreat":
+				currentAction._PossibilitySpace.Add(new Action("Advance"));
+				currentAction._PossibilitySpace.Add(new Action("LungeRecover"));
+				break;
+			case "LungeRecover":
+				currentAction._PossibilitySpace.Add(new Action("ParrySix"));
+				currentAction._PossibilitySpace.Add(new Action("Retreat"));
+				currentAction._PossibilitySpace.Add(new Action("DoubleRetreat"));
+				currentAction._PossibilitySpace.Add(new Action("ParryEight"));
+				currentAction._PossibilitySpace.Add(new Action("ParrySeven"));
+				break;
+			}
+		}
+	}
+
+	public string ChooseRandomResponse(string key){
+		for(int i = 0; i < _actionKeys.Count; i++){
+			Action currentAction = (Action)_actionKeys[i];
+			if(currentAction.name == key){
+				int randomChoice = Random.Range(0, currentAction._PossibilitySpace.Count);
+				return ((Action)currentAction._PossibilitySpace[randomChoice]).name;
+			}
+		}
+		Debug.Log("No reponses available.");
+		return "";
 	}
 }
  
