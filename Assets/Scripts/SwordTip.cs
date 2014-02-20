41cb5af4c480157b06100ab51cff9c33ce41b854
conovich <conovich@seas.upenn.edu>
2014-02-20 02:21:17 -0500

    Created SwordTip script to handle sword tip collisions.

---
+++ Assets/Scripts/SwordTip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordTip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//not working -- collider and joint 2 not moving/rotating
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("Hit something!");
+		if(collision.collider.tag == "opponent"){ //an opponent hit! this needs to be more "if it's player two..." or something more versatile.
+			Debug.Log("Hit opponent!"); //should tell gamestate to update score, lightbox to update lights
+		}
+	}
+}
