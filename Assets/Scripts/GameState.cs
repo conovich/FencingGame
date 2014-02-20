79b3ec671a08db41527eab269efd0e9e43ae138d
conovich <conovich@seas.upenn.edu>
2014-02-20 02:18:20 -0500

    Created a GameState singleton class for the overall game loop.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	private static GameState _instance;
+	public static GameState Instance{
+		get{return _instance;}
+	}
+
+	void Awake(){
+		if(_instance != null){
+			Debug.Log("Instance already exists.");
+			return;
+		}
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
