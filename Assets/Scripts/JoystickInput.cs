2c7b0320e778b3efcf9f2c25ac154400e022c7b0
conovich <conovich@seas.upenn.edu>
2014-02-22 00:16:46 -0500

    Commented out debug statements in joystick input debug script.

--- Assets/Scripts/JoystickInput.cs
+++ Assets/Scripts/JoystickInput.cs
@@ -11,16 +11,16 @@ public class JoystickInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("A Button")){
-			Debug.Log("YAY A");
+			//Debug.Log("YAY A");
 		}
 		if(Input.GetButtonDown("B Button")){
-			Debug.Log("YAY B");
+			//Debug.Log("YAY B");
 		}
 		if(Input.GetButtonDown("X Button")){
-			Debug.Log("YAY X");
+			//Debug.Log("YAY X");
 		}
 		if(Input.GetButtonDown("Y Button")){
-			Debug.Log("YAY Y");
+			//Debug.Log("YAY Y");
 		}
 		if(Input.GetAxis("X Axis") > 0){
 			//Debug.Log("HEYO +X AXIS" + Input.GetAxis("X Axis"));
