2b6dd0031dda1e45a9cacc039d1261f87e950793
conovich <conovich@seas.upenn.edu>
2014-03-09 18:05:51 -0400

    Added 'crosshares'. Which are apparently spelled 'crosshairs.' Whoops...

---
+++ Assets/Scripts/CrossHares.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossHares : MonoBehaviour {
+	public Material _OnTargetMat;
+	public Material _OffTargetMat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowOnTarget(){
+		gameObject.renderer.material = _OnTargetMat;
+	}
+
+	public void ShowOffTarget(){
+		gameObject.renderer.material = _OffTargetMat;
+	}
+}
