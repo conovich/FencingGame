91c0f189b90eb534a42b1543520d44f69b40c065
conovich <conovich@seas.upenn.edu>
2014-03-30 18:16:01 -0400

    Added camera follow abilities. Over-shoulder cam now follows player 1.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform Target;
+	public bool X;
+	public bool Y;
+	public bool Z;
+
+	Vector3 initDistance;
+
+	// Use this for initialization
+	void Start () {
+		initDistance = Target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveTo = Target.position - initDistance;
+		if(!X){
+			moveTo.x = transform.position.x;
+		}
+		if(!Y){
+			moveTo.y = transform.position.y;
+		}
+		if(!Z){
+			moveTo.z = transform.position.z;
+		}
+		transform.position = moveTo;
+	}
+}
