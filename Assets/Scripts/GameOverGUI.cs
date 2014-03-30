4317147694d541536c1467ea2c27626aeaa45a99
conovich <conovich@seas.upenn.edu>
2014-03-30 15:48:21 -0400

    Added GameOver scene.

---
+++ Assets/Scripts/GameOverGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverGUI : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		float buttonHeight = 30;
+		float buttonWidth = 200;
+		if(GUI.Button(new Rect(Screen.width/2 - buttonWidth/2, Screen.height/2 - buttonHeight/2, buttonWidth, buttonHeight), "PLAY AGAIN")){
+			Application.LoadLevel("MainWithFencers");
+		}
+
+		if(GUI.Button(new Rect(Screen.width/2 - buttonWidth/2, Screen.height/2 - buttonHeight/2 - buttonHeight, buttonWidth, buttonHeight), "RETURN TO MAIN MENU")){
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
