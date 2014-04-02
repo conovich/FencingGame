dae19bdbb23b86dee21e5a1adbb8219496da8840
conovich <conovich@seas.upenn.edu>
2014-04-02 14:13:25 -0400

    Created Text AI test scene with buttons for action selection.

---
+++ Assets/Scripts/TextTest/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	public string stringToEdit = "Type move here!";
+	public string actionString;
+
+	public GUIText actionLabel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		actionLabel.text = actionString;
+	}
+
+	void OnGUI(){
+		int buttonHeight = 30;
+		int buttonWidth = 60;
+
+		int numButton = 0;
+		if(GUI.Button(new Rect(10 + numButton*buttonWidth, Screen.height - buttonHeight, buttonWidth, buttonHeight), "engarde")){
+			actionString = "engarde";
+		}
+		numButton++;
+		if(GUI.Button(new Rect(10 + numButton*buttonWidth, Screen.height - buttonHeight, buttonWidth, buttonHeight), "lunge")){
+			actionString = "lunge";
+		}
+		numButton++;
+		if(GUI.Button(new Rect(10 + numButton*buttonWidth, Screen.height - buttonHeight, buttonWidth, buttonHeight), "parry 6")){
+			actionString = "parry 6";
+		}
+		numButton++;
+		if(GUI.Button(new Rect(10 + numButton*buttonWidth, Screen.height - buttonHeight, buttonWidth, buttonHeight), "parry 4")){
+			actionString = "parry 4";
+		}
+
+
+		/*stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+		Event e = Event.current;
+		bool userHasHitReturn = false;
+		if (e.keyCode == KeyCode.Return){
+			userHasHitReturn = true;
+		}
+		if(userHasHitReturn){
+			actionString = stringToEdit;
+			stringToEdit = "";
+			userHasHitReturn = false;
+		}*/
+	}
+}
---
+++ Assets/Scripts/TextTest/TextAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ParsePlayerAction();
+	}
+
+	void ParsePlayerAction(){
+
+	}
+}
