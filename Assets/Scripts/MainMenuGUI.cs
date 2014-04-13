e4a3c7103bd75c83df6bdc5d7c9ff1693557ec6d
conovich <conovich@seas.upenn.edu>
2014-04-12 23:05:48 -0400

    Added single vs. multiplayer toggles to main menu.

--- Assets/Scripts/MainMenuGUI.cs
+++ Assets/Scripts/MainMenuGUI.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class MainMenuGUI : MonoBehaviour {
+	bool isSingleplayer = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,13 @@ public class MainMenuGUI : MonoBehaviour {
 		float buttonWidth = 120;
 		if(GUI.Button(new Rect(Screen.width/2 - buttonWidth/2, Screen.height/2 - buttonHeight/2, buttonWidth, buttonHeight), "START MATCH")){
 			Application.LoadLevel("MainWithFencers");
+		}
+		//make multiplayer vs single player toggle here, to set gamestate's playerselection enum 
+		if(GUI.Toggle(new Rect(10, 40, 120, 20), isSingleplayer, "Single Player")){
+			isSingleplayer = true;
+		}
+		if(GUI.Toggle ( new Rect(10, 60, 120, 20), !isSingleplayer, "Multiplayer")){
+			isSingleplayer = false;
 		}
 	}
 }
