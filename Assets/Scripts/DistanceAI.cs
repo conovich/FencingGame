787f22d670e086592a43e70e482abb863df5bb5e
conovich <conovich@seas.upenn.edu>
2014-04-26 16:56:12 -0400

    Added scripts for distance-AI movement.

---
+++ Assets/Scripts/DistanceAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceAI : MonoBehaviour {
+	public Transform _PlayerFencer;
+
+	public float _LungeDistance;
+	public float _ExtensionDistance;
+	private int _distanceToPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameState.playerSelection = GameState.PlayerSelection.multiplayer){
+			GetDistanceToPlayer();
+		}
+	}
+
+	void GetDistanceToPlayer(){
+		_distanceToPlayer = (transform.position - _PlayerFencer.transform.position).magnitude();
+	}
+
+	//based on distance to player (like within lunge distance or extension distance...),
+	//...pick advance, retreat, double retreat, double advance, or lunge!
+	//then AI fencer should do this. like actually.
+	void ChooseMovement(){
+
+	}
+}
