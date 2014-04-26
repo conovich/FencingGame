979f7ada3f5c80dd95f234cb9935204f824dfbd3
conovich <conovich@seas.upenn.edu>
2014-04-26 16:37:39 -0400

    Tip targets only render in play.

---
+++ Assets/Scripts/TipTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameState.Instance.CurrentState == GameState.State.inPlay){
+			renderer.enabled = true;
+		}
+		else{
+			renderer.enabled = false;
+		}
+	}
+}
